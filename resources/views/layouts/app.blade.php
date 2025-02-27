ce3e3d0fe68c5784413642366afc1dfa633177d0
daniyalbutt <34391174+daniyalbutt@users.noreply.github.com>
2025-02-27 22:31:11 +0500

    Adding Mobile responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,10 @@
                     </span>
                 </a>
             </div>
+            <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
+                <i class="fa fa-fw fa-bars"></i>
+            </button>
+
         </div>
 
         <div class="d-flex">
