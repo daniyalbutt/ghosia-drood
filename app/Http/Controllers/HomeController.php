ad9cabbf8504289b661b6c40a29f813e040fb2e8
daniyalbutt <34391174+daniyalbutt@users.noreply.github.com>
2025-02-28 22:14:01 +0500

    Updating Current Month Durood.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $total_durood = Durood::whereMonth('created_at',\Carbon\Carbon::now()->month)
                    ->whereYear('created_at', \Carbon\Carbon::now()->year)
                    ->sum('durood');
-        $current_month = \Carbon\Carbon::now()->month;
+        $current_month = \Carbon\Carbon::now()->format('F');
         $total_user = DB::table('users')->where('status', 0)->where('is_admin', 0)->count();
         $total_book = DB::table('books')->where('status', 0)->count();
         $data = Durood::orderBy('updated_at', 'desc')->limit(10)->get();
